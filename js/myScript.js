08d440cebcf1d641915ef9dbd966a2c0c5685491
Aidario85 <89236332+Aidario85@users.noreply.github.com>
2021-09-27 13:02:11 +0600

    Сделал ДЗ, но не до конца
    Сделал ДЗ, но не до конца. Не смог сделать активный навигатор при прокрутке вниз. И не смог сделать 2 и 3 слайд с модальным окном.

--- js/myScript.js
+++ js/myScript.js
@@ -1,6 +1,5 @@
 "use strict"
 
-
 let type_select;
 
 do {
@@ -47,16 +46,20 @@ function varUsh(){
 siteType(type_select, varBir, varEki, varUsh, varJok);
 
 let typePrice;
+let typeDue;
 
  if (type_select == 1){
     typePrice = 5000;
-        
+    typeDue = 10;    
         }
          else if (type_select == 2){
-    typePrice = 3000;;
+    typePrice = 3000;
+    typeDue = 12;
              }  
           else if (type_select == 3){
-                typePrice = 6000;}
+          typePrice = 6000;
+          typeDue = 15;
+          }
 
 console.log (typePrice);
 
@@ -108,16 +111,21 @@ function varTres(){
 siteDesign(design_select, varUno, varDos, varTres, varNona);
 
 let designPrice;
+let designDue;
 
  if (design_select == 1){
     designPrice = 10000;
+    designDue = 5;
         
         }
          else if (design_select == 2){
-    designPrice = 20000;;
+    designPrice = 20000;
+    designDue = 8;
              }  
           else if (design_select == 3){
-                designPrice = 30000;}
+    designPrice = 30000;
+    designDue = 15;       
+          }
 
 console.log (designPrice);
 
@@ -170,25 +178,171 @@ siteAdapt(adapt_select, varIch, varNi, varSan, varHach);
 
 let adaptPrice;
 
+let adaptDue;
+
  if (adapt_select == 1){
     adaptPrice = 100;
-        
+    adaptDue = 1;    
         }
          else if (adapt_select == 2){
-    adaptPrice = 2000;;
+    adaptPrice = 2000;
+    adaptDue = 2;         
              }  
           else if (adapt_select == 3){
-                adaptPrice = 3000;}
+                adaptPrice = 3000;
+              adaptDue = 3;
+          
+          }
 
 console.log (adaptPrice);
+////////////////////////////////////////////////////////
 
 
 let totalSum = (typePrice + designPrice + adaptPrice);
+let totalDue = (adaptDue + designDue + typeDue);
 
-let priceTotal = confirm ("Стоимость Вашего сайта будет составлять  " + totalSum + " рублей");
-
-
-
+let priceTotal = confirm ("Стоимость Вашего сайта будет составлять  " + totalSum + " рублей " + ", срок выполнения " + totalDue + " (дня)дней");
 
 
+$('#dueDate').text(totalDue + " дн.");
+$('#totalPrice').text(totalSum + " руб.");
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////
+ let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.numberBir');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+   
+  function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberOne');
+       } 
+    });
+ 
+var currentNumber = $('.numberOne').text();
+
+$({numberValue: currentNumber}).animate({numberValue: 120}, {
+    duration: 2000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberOne').text(Math.ceil(this.numberValue)); 
+    }
+});
+    }
+    /////////////////////////////////////////////////
+   let optionTwo = {threshold:[0.5]};
+    let observerTwo = new IntersectionObserver(onEntryTwo, optionTwo);
+    let elementTwo = $('.numberEki');
+    elementTwo.each((i, el) => {
+        observerTwo.observe(el);
+    });
+   
+  function onEntryTwo (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberTwo');
+        } 
+    });
+
+var currentNumberTwo = $('.numberTwo').text();
+
+$({numberValue: currentNumberTwo}).animate({numberValue: 4600}, {
+    duration: 8000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberTwo').text(Math.floor(this.numberValue)); 
+    }
+});
+    }
+///////////////////////////////////////////////////////
+  let optionThree = {threshold:[0.5]};
+    let observerThree = new IntersectionObserver(onEntryThree, optionThree);
+    let elementThree = $('.numberUsh');
+    elementThree.each((i, el) => {
+        observerThree.observe(el);
+    });
+   
+  function onEntryThree (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberThree');
+        } 
+    });
+ 
+var currentNumberThree= $('.numberThree').text();
+
+$({numberValue: currentNumberThree}).animate({numberValue: 341}, {
+    duration: 3000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberThree').text(Math.floor(this.numberValue)); 
+    }
+});
+}
+///////////////////////////////////////////////////////
+  let optionFour = {threshold:[0.5]};
+    let observerFour = new IntersectionObserver(onEntryFour, optionFour);
+    let elementFour = $('.numberTort');
+    elementFour.each((i, el) => {
+        observerFour.observe(el);
+    });
+   
+  function onEntryFour(entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberFour');
+        } 
+    });
+
+var currentNumberFour= $('.numberFour').text();
+
+$({numberValue: currentNumberFour}).animate({numberValue: 24}, {
+    duration: 6000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberFour').text(Math.floor(this.numberValue)); 
+    }
+});
+ 
+    }
+
+////////////////////////////////////////////////////////////////////////
+
+
+
+ $(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    
+    $(".sect").each((i, el) => {
+        
+        if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+            $("nav a").each((i, el) => {
+                if ($(el).hasClass("active")){
+                    $(el).removeClass("active");
+                }
+            });
+            $('nav li:eq('+ i +')').find('a').addClass('active');
+        }
+    });
+});
+
+//////////////////////////////////////////////////////
+
+    $('a[href^="#"]').click(function () {
+            let valHref = $(this).attr("href");
+            $('html, body').animate({
+                scrollTop: $(valHref).offset().top - 43 + "px"
+            });
+        })
+/////////////////////////////////////////////////
+ var delay_popup = 5000;
+    setTimeout("document.getElementById('bg_popup').style.display='block'", delay_popup);
 
---
+++ js/testScript.js
@@ -0,0 +1,143 @@
+"use strict"
+$(document).ready(function(){
+    //working!
+    /*
+        let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+   
+  function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        } 
+    });
+    
+}
+*/
+    
+/////////////////////////////////////////////////////////////////////////
+//working!
+    let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.numberBir');
+    elements.each((i, el) => {
+        observer.observe(el);
+    });
+   
+  function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberOne');
+            
+        } 
+    });
+    
+
+    
+var currentNumber = $('.numberOne').text();
+
+$({numberValue: currentNumber}).animate({numberValue: 120}, {
+    duration: 2000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberOne').text(Math.ceil(this.numberValue)); 
+    }
+});
+     
+      
+    }
+    
+    
+    /////////////////////////////////////////////////
+   let optionTwo = {threshold:[0.5]};
+    let observerTwo = new IntersectionObserver(onEntryTwo, optionTwo);
+    let elementTwo = $('.numberEki');
+    elementTwo.each((i, el) => {
+        observerTwo.observe(el);
+    });
+   
+  function onEntryTwo (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('numberTwo');
+        } 
+    });
+    
+
+    
+var currentNumberTwo = $('.numberTwo').text();
+
+$({numberValue: currentNumberTwo}).animate({numberValue: 4600}, {
+    duration: 8000,
+    easing: 'linear',
+    step: function() { 
+        $('.numberTwo').text(Math.floor(this.numberValue)); 
+    }
+});
+      
+      
+    }
+
+//////////////////////////////////////////////////////////////////////////
+/* 
+//not working
+$(window).scroll(() => {
+    let scrollDistance = $(window).scrollTop();
+    
+    $(".section").each((i, el) => {
+        
+        if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+            $("nav a").each((i, el) => {
+                if ($(el).hasClass ("active")){
+                    $(el).removeClass("active");
+                }
+            });
+            $('nav li:eq('+ i +')').find('a').addClass('active');
+        }
+    });
+});
+*/
+////////////////////////////////////////////////////////////////////////
+});
+
+/* let options = {threshold:[0.5]};
+    let observer = new IntersectionObserver(onEntry, options);
+    let elements = $('.element-animation');
+    elements.each((i,el) => {
+        observer.observe(el);
+    });
+   
+  function onEntry (entry){
+    entry.forEach(change => {
+        if (change.isIntersecting){
+            change.target.classList.add('show-animation');
+        } 
+    });
+    
+}
+
+    
+    
+    
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////
+    //working!
+    $('a[href^="#"]').click(function () {
+            let valHref = $(this).attr("href");
+            $('html, body').animate({
+                scrollTop: $(valHref).offset().top - 20 + "px"
+            });
+        })
+/////////////////////////////////////////////////////////////////////
+    
+ $('.image-link').magnificPopup({type:'image'});
+});*/
+  
